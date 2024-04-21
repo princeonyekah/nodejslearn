a1998ab9e5d15e1375a166b212f313b2560d57c8
Princewill <princewill.onyekah@kibo.school>
2024-04-21 23:18:34 +0100

    update task

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,13 +1,16 @@
 
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://ponyekah:1234@nodeexpressprojects.ymso2aa.mongodb.net/?retryWrites=true&w=majority'
-mongoose
-    .connect(connectionString,{
-        useNewUrlParser: true,
-        useCreateIndex:true,
-        useFindAndModify:false,
-        useUnifiedTopology:true,
-    }
-   )
-    .then(()=> console.log('CONNECTED TO THE DB...'))
-    .catch((err) => console.log(err) )
+
+
+const connectDB = (url) =>{
+    return mongoose
+            .connect(url,{
+                useNewUrlParser: true,
+                useCreateIndex:true,
+                useFindAndModify:false,
+                useUnifiedTopology:true,
+            }
+        )
+}
+
+module.exports = connectDB
